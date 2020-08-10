d35abcfa8db801445d1f862a653f2992bbd167c4
ramy <ramy.ferhat@epitech.eu>
2020-08-10 08:31:18 -0400

    First commit

---
+++ my_strncpy.c
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2019
+** my_strncpy.c
+** File description:
+** my_strncpy
+*/
+
+char *my_strncpy(char *dest, char const *src, int n)
+{
+    int i = 0;
+
+    for (; src[i] != n; i++) {
+        dest[i]=src[i];
+    }
+    
+}
